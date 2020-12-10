646a39f7d89d3182521b96d308c1e89ef504d273
Matt Bentley <mbentley@mbentley.net>
2020-12-10 09:06:40 -0500

    comments are good
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- jwt_license_decode.sh
+++ jwt_license_decode.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 
-invalid_license() {
-  echo "ERROR: invalid license"
-  exit 1
-}
-
+# make sure a parameter (license) was passed
 if [ -z "${1}" ]
 then
   echo "ERROR: no license data sent as first parameter"
   exit 1
 fi
 
+# break apart the JWT into the main parts; send errors to null (gets past padding errors)
 JWT_HEADER="$(echo "${1}" | awk -F '.' '{print $1}' | sed 's/-/+/g; s/_/\//g' | base64 -d 2>/dev/null)"
 JWT_PAYLOAD="$(echo "${1}" | awk -F '.' '{print $2}' | sed 's/-/+/g; s/_/\//g' | base64 -d 2>/dev/null)"
 JWT_SIGNATURE="$(echo "${1}" | awk -F '.' '{print $3}' | tr -d '%' | sed 's/-/+/g; s/_/\//g' | base64 -d 2>/dev/null)"
 
+# validate that we were able to decode all 3 portions
 if [ -z "${JWT_HEADER}" ] || [ -z "${JWT_PAYLOAD}" ] || [ -z "${JWT_SIGNATURE}" ]
 then
   echo "ERROR: invalid license or unable to parse"
   exit 1
 fi
 
+# output the raw details
 echo "Raw License Details:"
 echo "${JWT_HEADER}"
 echo "${JWT_PAYLOAD}"
 echo
 
+# output the pretty jq formatted details
 echo "Pretty details:"
 echo "${JWT_HEADER}" | jq .
 echo "${JWT_PAYLOAD}" | jq .
 echo
 
+# output the valid dates
 echo "Not Valid Before: $(date -d @"$(echo "${JWT_PAYLOAD}" | jq .iat)")"
 echo "Not Valid After:  $(date -d @"$(echo "${JWT_PAYLOAD}" | jq .exp)")"
